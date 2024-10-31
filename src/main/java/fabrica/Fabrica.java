64ee0cc371c9cfb892953a84c6bdea30b6efa670
Javier Osuna <javiosuna01@gmail.com>
2024-10-31 13:22:31 +0100

    Creación Rama Javi

--- src/main/java/fabrica/Fabrica.java
+++ src/main/java/fabrica/Fabrica.java
@@ -12,30 +12,6 @@ public class Fabrica {
     private Pieza pieza;
     private static Random alea = new Random();
 
-    /// Lista del ejemplo - Si la quieren sacar por favor comentar ya que se puede reutilizar en el main!
-    public Fabrica() {
-        pieza = new Pieza();
-        maquinas = new ArrayList<>();
-        maquinas.add(new Fresadora(Posicion.IzSu, OrFresa.Vertical, Grosor.Medio));
-        maquinas.add(new Fresadora(Posicion.IzCe, OrFresa.Diagonal, Grosor.Medio));
-        maquinas.add(new Fresadora(Posicion.IzIn, OrFresa.Vertical,Grosor.Medio));
-        maquinas.add(new Rotadora(Sentido.Antihorario));
-        maquinas.add(new Lijadora(Posicion.CeSu, OrLija.Norte,Grosor.Fino));
-        maquinas.add(new Fresadora(Posicion.CeSu,OrFresa.Diagonal, Grosor.Grueso));
-        maquinas.add(new Rotadora(Sentido.Antihorario));
-        maquinas.add(new Fresadora(Posicion.CeCe, OrFresa.Diagonal,Grosor.Medio));
-        maquinas.add(new Taladradora(Posicion.IzCe,Grosor.Medio));
-        maquinas.add(new Rotadora(Sentido.Antihorario));
-        maquinas.add(new Fresadora(Posicion.CeSu, OrFresa.Vertical, Grosor.Fino));
-        maquinas.add(new Fresadora(Posicion.IzIn, OrFresa.Vertical, Grosor.Medio));
-        maquinas.add(new Rotadora(Sentido.Horario));
-        maquinas.add(new Lijadora(Posicion.IzCe, OrLija.Sur,Grosor.Grueso));
-        maquinas.add(new Fresadora(Posicion.CeCe, OrFresa.Diagonal, Grosor.Fino));
-        maquinas.add(new Lijadora(Posicion.IzSu, OrLija.Norte,Grosor.Medio));
-        maquinas.add(new Fresadora(Posicion.IzCe, OrFresa.Vertical, Grosor.Medio));
-        maquinas.add(new Fresadora(Posicion.IzSu,OrFresa.Diagonal, Grosor.Grueso));
-    }
-
     public Fabrica(List<Maquina> maquinas) {
         pieza = new Pieza();
         this.maquinas = maquinas;
